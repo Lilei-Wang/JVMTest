ee0a29d035408d74957fb6fc4d7e9ae2c573deb1
Lilei-Wang <767034609@qq.com>
2019-06-04 16:30:56 +0800

    初始版本

---
+++ src/HeapOOM.java
@@ -0,0 +1,20 @@
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * VM args:-Xms20m -Xmx20m -XX:+HeapDumpOnOutOfMemoryError
+ * @author Administrator
+ *
+ */
+public class HeapOOM {
+	static class OOMObject{
+		
+	}
+	
+	public static void main(String[] args) {
+		List<OOMObject> list=new ArrayList<OOMObject>();
+		while (true) {
+			list.add(new OOMObject());
+		}
+	}
+}
